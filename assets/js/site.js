a70a59ae683c809918a825ce348e233aad32fd26
bo-nicolaisen <31656337+bo-nicolaisen@users.noreply.github.com>
2023-10-04 16:15:49 +0200

    async fetch in modules

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,12 @@
-function
+import { getAllUsers,getUserById } from "./modules/model/dummyjasonLib.js";
+
+
+/* getAllUsers(0,0).then((myUsers)=>{
+    console.log(myUsers);
+}); */
+
+
+
+getUserById('1').then((myUser)=>{
+    console.log(myUser);
+});
